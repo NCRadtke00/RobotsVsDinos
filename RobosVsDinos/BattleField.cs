fd9bec0ca91681a2dfa835ca99f3f1bae06d449d
NCRadtke00 <raddnc@gmail.com>
2020-08-10 10:07:58 -0500

    created classes put in basic functions

---
+++ RobosVsDinos/BattleField.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobosVsDinos
+{
+    class BattleField
+    {
+        public void RunGame()
+        {
+
+            if(Robot.health == 0 || Dinosaur.health == 0)
+            {
+                Console.WriteLine("The Dinosaur heard has over run the robots!");
+            }
+            else
+            {
+                Console.WriteLine("The Robot fleet has slaughtered the Dinosaurs!");
+            }
+        }
+    }
+}
---
+++ RobosVsDinos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobosVsDinos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BattleField battleField = new BattleField();
+            battleField.RunGame();
+        }
+    }
+}
